0bf43a28a7273db9f28a76f00c5c33d3b22bd7ca
Tebrica Bole <avramsmart@gmail.com>
2017-09-20 18:59:27 +0200

    adding infinite loop for updating games scores

--- football_daily_stats.py
+++ football_daily_stats.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+import time
+
 
 browser = webdriver.Chrome(executable_path='./webdrivers/chromedriver')
 browser.implicitly_wait(15)
@@ -7,8 +9,8 @@ browser.get('https://www.rezultati.com/')
 field_utakmice = browser.find_element_by_id('fs')
 tables = field_utakmice.find_elements_by_class_name('soccer')
 
-def update_feed():
-    for table in tables:
+def update_feed(a=None, b=None):
+    for table in tables[a:b]:
         thead = table.find_element_by_tag_name('thead')
         text = thead.text.split('\n')
         if len(text) > 1:
@@ -22,3 +24,13 @@ def update_feed():
             print(row) # resultats
 
 update_feed()
+a, b = 0, 0
+while True:
+    time.sleep(3)
+    b += 10
+    # update one part at a time, 10 leagues in this case
+    update_feed(a, b)
+    a = b
+    if b > len(tables):
+        # reset a and b to repeat the process
+        a, b = 0, 0
